330377edca284b44d43901012f20bb34d69e54fb
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-14 09:03:45 -0300

    [FIX] - update product bug mapper

--- src/main/java/com/vitoriadeveloper/catolog_api/services/ProductService.java
+++ src/main/java/com/vitoriadeveloper/catolog_api/services/ProductService.java
@@ -1,7 +1,11 @@
 package com.vitoriadeveloper.catolog_api.services;
 
+import com.vitoriadeveloper.catolog_api.domain.Category;
 import com.vitoriadeveloper.catolog_api.domain.Product;
 import com.vitoriadeveloper.catolog_api.domain.exceptions.CategoryNotFoundException;
+import com.vitoriadeveloper.catolog_api.dto.ProductRequestDTO;
+import com.vitoriadeveloper.catolog_api.mappers.ProductMapper;
+import com.vitoriadeveloper.catolog_api.repositories.CategoryRepository;
 import com.vitoriadeveloper.catolog_api.repositories.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +15,13 @@ import java.util.Optional;
 @Service
 public class ProductService {
     private final ProductRepository repository;
+    private final CategoryRepository categoryRepository;
+    private final ProductMapper mapper;
 
-    public ProductService(ProductRepository repository) {
+    public ProductService(ProductRepository repository, CategoryRepository categoryRepository,  ProductMapper mapper) {
         this.repository = repository;
+        this.categoryRepository = categoryRepository;
+        this.mapper = mapper;
     }
 
     public List<Product> list() {
@@ -21,19 +29,29 @@ public class ProductService {
     }
 
     public Product create(Product product) {
-        return this.repository.save(product);
+        if (product.getCategory() != null && product.getCategory().getId() != null) {
+            Category category = categoryRepository.findById(product.getCategory().getId())
+                    .orElseThrow(() -> new CategoryNotFoundException(product.getCategory().getId()));
+            product.setCategory(category);
+        }
+        return repository.save(product);
     }
 
-    public Optional<Product> update(Product product, String id) {
-        return this.repository.findById(id).map(existingProduct -> {
-            existingProduct.setTitle(product.getTitle());
-            existingProduct.setPrice(product.getPrice());
-            if (product.getCategory() != null) {
-                existingProduct.setCategory(product.getCategory());
-            }
-            existingProduct.setDescription(product.getDescription());
-            return repository.save(existingProduct);
-        });
+    public Optional<Product> update(ProductRequestDTO dto, String id) {
+        return repository.findById(id)
+                .map(existingProduct -> {
+                    // att o produto existente usando o mapper
+                    mapper.updateProductFromDto(dto, existingProduct);
+
+                    // se categoria foi enviada, busca a entidade completa
+                    if (existingProduct.getCategory() != null && existingProduct.getCategory().getId() != null) {
+                        Category category = categoryRepository.findById(existingProduct.getCategory().getId())
+                                .orElseThrow(() -> new CategoryNotFoundException(existingProduct.getCategory().getId()));
+                        existingProduct.setCategory(category);
+                    }
+
+                    return repository.save(existingProduct);
+                });
     }
 
     public void deleteById(String id) {
