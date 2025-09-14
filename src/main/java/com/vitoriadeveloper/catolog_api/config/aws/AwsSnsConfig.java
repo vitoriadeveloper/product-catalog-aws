8427d5f13160911000327e68f225b103a60087b4
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-14 09:44:38 -0300

    [FEAT] - add aws sns congig

---
+++ src/main/java/com/vitoriadeveloper/catolog_api/config/aws/AwsSnsConfig.java
@@ -0,0 +1,38 @@
+package com.vitoriadeveloper.catolog_api.config.aws;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sns.SnsClient;
+
+public class AwsSnsConfig {
+
+    @Configuration
+    public class AWSSNSConfig {
+        @Value("${aws.region}")
+        private String region;
+        @Value("${aws.accessKeyId}")
+        private String accessKeyId;
+        @Value("${aws.secretKey}")
+        private String secretKey;
+        @Value("${aws.sns.topic.catalog.arn}")
+        private String catalogTopic;
+
+
+        @Bean
+        public SnsClient amazonSNSbuilder() {
+            return SnsClient.builder()
+                    .region(Region.of(region))
+                    .credentialsProvider(
+                            StaticCredentialsProvider.create(
+                                    AwsBasicCredentials.create(accessKeyId, secretKey)
+                            )
+                    ).build();
+        }
+
+    }
+
+}
