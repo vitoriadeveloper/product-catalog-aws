56877cde121c62caee885452f3a17debf5bd7de3
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-13 21:05:46 -0300

    [FEAT] - add validations at product entity

--- src/main/java/com/vitoriadeveloper/catolog_api/domain/Product.java
+++ src/main/java/com/vitoriadeveloper/catolog_api/domain/Product.java
@@ -1,5 +1,6 @@
 package com.vitoriadeveloper.catolog_api.domain;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,9 +15,16 @@ public class Product {
     @Id
     private String id;
 
+    @NotBlank(message = "O título é obrigatório")
     private String title;
+
+    @NotBlank(message = "O restaurante dono é obrigatório")
     private String owner;
+
+    @NotBlank(message = "O preço é obrigatório para o produto")
     private Double price;
+
+    @NotBlank(message = "A descrição do produto é obrigatória")
     private String description;
 
     private Category category;
