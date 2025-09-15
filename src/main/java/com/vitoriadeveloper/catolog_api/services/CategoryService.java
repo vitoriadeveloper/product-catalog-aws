f5512f51381fe558a8c5fa48f56426759d65b3ad
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2025-09-15 19:44:13 -0300

    [FEAT] - implementing awsSnsService at category service

--- src/main/java/com/vitoriadeveloper/catolog_api/services/CategoryService.java
+++ src/main/java/com/vitoriadeveloper/catolog_api/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.vitoriadeveloper.catolog_api.services;
 
 import com.vitoriadeveloper.catolog_api.domain.Category;
 import com.vitoriadeveloper.catolog_api.domain.exceptions.CategoryNotFoundException;
+import com.vitoriadeveloper.catolog_api.dto.AwsMessageCategoryDTO;
 import com.vitoriadeveloper.catolog_api.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +12,12 @@ import java.util.Optional;
 @Service
 public class CategoryService {
     private final CategoryRepository repository;
+    private final AwsSnsService awsSnsService;
 
-    public CategoryService(CategoryRepository repository) {
+    // TO DO dto aws p category
+    public CategoryService(CategoryRepository repository, AwsSnsService awsSnsService) {
         this.repository = repository;
+        this.awsSnsService = awsSnsService;
     }
 
     public List<Category> list() {
@@ -21,14 +25,32 @@ public class CategoryService {
     }
 
     public Category create(Category category) {
-        return this.repository.save(category);
+        Category created = this.repository.save(category);
+        AwsMessageCategoryDTO message = new AwsMessageCategoryDTO(
+                created.getId(),
+                created.getTitle(),
+                created.getDescription(),
+                created.getOwnerId()
+        );
+
+        awsSnsService.publishCategory(message);
+        return created;
     }
 
     public Optional<Category> update(Category category, String id) {
         return this.repository.findById(id).map(existingCategory -> {
             existingCategory.setTitle(category.getTitle());
             existingCategory.setDescription(category.getDescription());
-            return repository.save(existingCategory);
+            Category updated = repository.save(existingCategory);
+            AwsMessageCategoryDTO message = new AwsMessageCategoryDTO(
+                    existingCategory.getId(),
+                    existingCategory.getTitle(),
+                    existingCategory.getDescription(),
+                    existingCategory.getOwnerId()
+            );
+
+            awsSnsService.publishCategory(message);
+            return updated;
         });
     }
 
@@ -37,5 +59,6 @@ public class CategoryService {
             throw new CategoryNotFoundException(id);
         }
         repository.deleteById(id);
+        awsSnsService.publishCategory(new AwsMessageCategoryDTO(id, null, null, null));
     }
 }
